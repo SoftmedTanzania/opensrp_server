3d8841115845752b4104310bec11d3d43b6fd442
cozej4 <ilakozejumanne@gmail.com>
2019-08-26 21:53:30 +0300

    MInor bugs fixes

--- opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/OpenmrsUserService.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/OpenmrsUserService.java
@@ -147,8 +147,6 @@ public class OpenmrsUserService extends OpenmrsService{
 		JSONObject object =  new JSONObject(op.body());
 		JSONArray results = object.getJSONArray("results");
 
-		logger.info("Results : "+results);
-
 		int size = results.length();
 		for(int i=0;i<size;i++){
 			try {
@@ -172,6 +170,9 @@ public class OpenmrsUserService extends OpenmrsService{
 
 		JSONArray teamMembers = new JSONArray();
 		JSONArray results = object.getJSONArray("results");
+
+
+		logger.info("Results : "+results);
 
 		int size = results.length();
 		for(int i=0;i<size;i++){
