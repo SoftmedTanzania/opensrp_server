30b6344e7c61b831530f9e43d47ce3d6f5cbb644
cozej4 <ilakozejumanne@gmail.com>
2019-08-27 19:06:45 +0300

    Code cleaning

--- opensrp-core/src/main/java/org/opensrp/service/GoogleFCMService.java
+++ opensrp-core/src/main/java/org/opensrp/service/GoogleFCMService.java
@@ -11,35 +11,29 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GoogleFCMService {
 
-	public void SendPushNotification(JSONObject msg, JSONArray to,boolean toFacilityUsers){
-		try {
-			String androidFcmKey;
-			if(toFacilityUsers) {
-				//Facility client app FCM server key
-				androidFcmKey = "AAAAEgpJVug:APA91bGCXo-2ut2gyxSmvOmSYSV_0W_RenJO-nkvqmD8wI-39LB0PQzRCMJxqtHKv0lyK22LrEwM3S4V8YJYlC3xKfqLo8pobONQ0fV9WNRqeJhvxe5F19-ql24THsqQ2ePVmp73mlyb";
-			}else{
-				//CHW app FCM server key
-				androidFcmKey = "AAAAEgpJVug:APA91bGCXo-2ut2gyxSmvOmSYSV_0W_RenJO-nkvqmD8wI-39LB0PQzRCMJxqtHKv0lyK22LrEwM3S4V8YJYlC3xKfqLo8pobONQ0fV9WNRqeJhvxe5F19-ql24THsqQ2ePVmp73mlyb";
-			}
-			String androidFcmUrl="https://fcm.googleapis.com/fcm/send";
+    public void SendPushNotification(JSONObject msg, JSONArray to, boolean toFacilityUsers) {
+        try {
+            String androidFcmKey;
+            androidFcmKey = "AAAAEgpJVug:APA91bGCXo-2ut2gyxSmvOmSYSV_0W_RenJO-nkvqmD8wI-39LB0PQzRCMJxqtHKv0lyK22LrEwM3S4V8YJYlC3xKfqLo8pobONQ0fV9WNRqeJhvxe5F19-ql24THsqQ2ePVmp73mlyb";
+            String androidFcmUrl = "https://fcm.googleapis.com/fcm/send";
 
-			RestTemplate restTemplate = new RestTemplate();
-			HttpHeaders httpHeaders = new HttpHeaders();
-			httpHeaders.set("Authorization", "key=" + androidFcmKey);
-			httpHeaders.set("Content-Type", "application/json");
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set("Authorization", "key=" + androidFcmKey);
+            httpHeaders.set("Content-Type", "application/json");
 
-			JSONObject json = new JSONObject();
+            JSONObject json = new JSONObject();
 
-			json.put("data", msg);
-			json.put("registration_ids", to);
+            json.put("data", msg);
+            json.put("registration_ids", to);
 
-			System.out.println("FCM Data:"+json.toString());
+            System.out.println("FCM Data:" + json.toString());
 
-			HttpEntity<String> httpEntity = new HttpEntity<String>(json.toString(),httpHeaders);
-			String response = restTemplate.postForObject(androidFcmUrl,httpEntity,String.class);
-			System.out.println("FCM:"+response);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-	}
+            HttpEntity<String> httpEntity = new HttpEntity<String>(json.toString(), httpHeaders);
+            String response = restTemplate.postForObject(androidFcmUrl, httpEntity, String.class);
+            System.out.println("FCM:" + response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
